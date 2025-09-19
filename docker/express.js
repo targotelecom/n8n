94749e0d02a54377d42ed8f6bc7aefc16f541ab5
targotelecom <59179367+targotelecom@users.noreply.github.com>
2025-09-19 14:22:22 -0400

    Update express.js

--- docker/express.js
+++ docker/express.js
@@ -17,7 +17,7 @@ var EventEmitter = require('node:events').EventEmitter;
 var mixin = require('merge-descriptors');
 var proto = require('./application');
 var Route = require('./router/route');
-var Router = require('./router');
+var Router = require('router');
 var req = require('./request');
 var res = require('./response');
 
